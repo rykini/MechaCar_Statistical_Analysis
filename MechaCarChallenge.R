9d4be7debb0a63fdee7176050062f32a0706cfab
rykini <ricky.ni@hotmail.com>
2021-06-13 19:10:59 -0400

    d2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+# deliverable 1
+
 # step 3
 library(dplyr)
 
@@ -9,3 +11,14 @@ car_ln_reg<- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_c
 
 # step 6
 summary(car_ln_reg)
+
+# deliverable 2
+
+# step 2
+sus_data <- read.csv("dataset/Suspension_Coil.csv", stringsAsFactors = F, check.names = F)
+
+# step 3
+total_summary <- sus_data %>% summarize(mean=mean(PSI), median=median(PSI), variance=var(PSI), stdev=sd(PSI))
+
+# step 4
+lot_summary <- sus_data %>% group_by(Manufacturing_Lot) %>% summarize(mean=mean(PSI), median=median(PSI), Variance=var(PSI), stdev=sd(PSI))
